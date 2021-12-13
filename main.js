1b3001eed920a8e03b524e8dd8223361d2f1714f
Andrey <maoltin@gmail.com>
2021-12-13 16:47:53 +0300

    initial

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    main();
+});
+
+function main() {
+	const qrcode = document.getElementById("qrcode");
+	const textInput = document.getElementById("text");
+	
+	const qr = new QRCode(qrcode);
+	
+	textInput.oninput = (e) => {
+		qr.makeCode(e.target.value.trim());
+	};
+	
+	qr.makeCode(textInput.value.trim());
+}
